66553b866ea4e7093f48ba69a149d8bc7cd26a35
Lurie1082 <89157845+Lurie1082@users.noreply.github.com>
2022-03-16 19:07:39 -0300

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,95 @@
+const preguntas = [
+    {
+      titulo: "¿En que año debuto Diego Maradona en Argentinos Junior?",
+      opciones: [
+        { textoRespuesta: "20 de octubre de 1976", isCorrect: true },
+        { textoRespuesta: "20 de julio de 1979", isCorrect: false },
+        { textoRespuesta: "30 de octubre de 1976", isCorrect: false },
+        { textoRespuesta: "15 de marzo de 1986", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "Cuantos goles hizo Maradona en su paso por Boca?",
+      opciones: [
+        { textoRespuesta: "48", isCorrect: false },
+        { textoRespuesta: "28", isCorrect: true },
+        { textoRespuesta: "166", isCorrect: false },
+        { textoRespuesta: "8", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "¿Como se llamaba el Jugador del Atletico de Madrid que le quebro el tobillo?",
+      opciones: [
+        { textoRespuesta: "Andoni Goikoetxea", isCorrect: true },
+        { textoRespuesta: "Sergio Goycochea", isCorrect: false },
+        { textoRespuesta: "Julio Olarticoechea", isCorrect: false },
+        { textoRespuesta: "El Vazco Arruabarrena", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "¿A que seleccion le hizo el gol , denominado 'La mano de dios'?",
+      opciones: [
+        { textoRespuesta: "Grecia", isCorrect: false },
+        { textoRespuesta: "Italia", isCorrect: false },
+        { textoRespuesta: "Belgica", isCorrect: false },
+        { textoRespuesta: "Inglaterra", isCorrect: true },
+      ],
+    },
+    {
+      titulo: "¿Cuantos goles hizo en la seleccion Argentina?",
+      opciones: [
+        { textoRespuesta: "44", isCorrect: false },
+        { textoRespuesta: "24", isCorrect: false },
+        { textoRespuesta: "134", isCorrect: false },
+        { textoRespuesta: "34", isCorrect: true },
+      ],
+    },
+    {
+      titulo: "¿Cuantos titulos gano con la seleccion argentina?",
+      opciones: [
+        { textoRespuesta: "1", isCorrect: false },
+        { textoRespuesta: "2", isCorrect: false },
+        { textoRespuesta: "4", isCorrect: false },
+        { textoRespuesta: "3", isCorrect: true },
+      ],
+    },
+    {
+      titulo: "¿Cuantos balones de oro obtuvo en su carrera?",
+      opciones: [
+        { textoRespuesta: "Dos", isCorrect: false },
+        { textoRespuesta: "Uno", isCorrect: false },
+        { textoRespuesta: "Diez", isCorrect: false },
+        { textoRespuesta: "Ninguno", isCorrect: true },
+      ],
+    },
+    {
+      titulo: "¿Que tiene tatuado en sus dos antebrazos?",
+      opciones: [
+        { textoRespuesta: "Dalma y Gianina", isCorrect: true },
+        { textoRespuesta: "La Pelota no se mancha", isCorrect: false },
+        { textoRespuesta: "Un retrato del che Guevara", isCorrect: false },
+        { textoRespuesta: "Un retrato de Fidel Castro", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "¿Cuantos Partidos disputo en los mundiales?",
+      opciones: [
+        { textoRespuesta: "21", isCorrect: true },
+        { textoRespuesta: "25", isCorrect: false },
+        { textoRespuesta: "31", isCorrect: false },
+        { textoRespuesta: "18", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "¿Cuantos goles hizo en los mundiales que jugo?",
+      opciones: [
+        { textoRespuesta: "8", isCorrect: true },
+        { textoRespuesta: "34", isCorrect: false },
+        { textoRespuesta: "5", isCorrect: false },
+        { textoRespuesta: "7", isCorrect: false },
+      ],
+    },
+  ];
+  
+  export default preguntas;
+  
